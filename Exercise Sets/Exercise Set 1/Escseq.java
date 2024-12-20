8e865daa24e4cf556eb926277d01bd2a31dfa124
cyparadox <167673884+cyparadox@users.noreply.github.com>
2024-12-20 04:50:49 -0800

    Update Escseq.java

---
+++ Exercise Sets/Exercise Set 1/Escseq.java
@@ -0,0 +1,11 @@
+public class Escseq {
+	public static void main(String[] args) {
+	
+			System.out.println(7);
+			System.out.println("\t14");
+			System.out.println(21);
+			System.out.println("\t28");
+			System.out.println(35);
+			//sorry po di nakaloop, sa phone po ako nagcode .-.
+	}
+}
