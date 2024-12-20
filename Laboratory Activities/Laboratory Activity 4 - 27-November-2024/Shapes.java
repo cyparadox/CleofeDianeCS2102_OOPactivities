3571d026cfd96b560056070bc0f812f490b0f6bd
cyparadox <167673884+cyparadox@users.noreply.github.com>
2024-12-20 04:59:54 -0800

    Update Shapes.java

---
+++ Laboratory Activities/Laboratory Activity 4 - 27-November-2024/Shapes.java
@@ -0,0 +1,6 @@
+// Shape Interface
+public interface Shapes {
+    double getArea();         // Method to calculate the area
+    double getPerimeter();    // Method to calculate the perimeter
+    void printShapeType();    // Method to print the shape type
+}
