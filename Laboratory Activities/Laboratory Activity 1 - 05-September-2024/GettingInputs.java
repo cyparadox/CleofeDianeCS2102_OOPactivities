a38c78a186a12ace7564df2fb72489d33bb637d2
cyparadox <167673884+cyparadox@users.noreply.github.com>
2024-12-20 04:55:32 -0800

    Update GettingInputs.java

---
+++ Laboratory Activities/Laboratory Activity 1 - 05-September-2024/GettingInputs.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GettingInputs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the year: ");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter the genre: ");
+        String genre = scanner.nextLine();
+
+        System.out.print("Enter the album: ");
+        String album = scanner.nextLine();
+
+        System.out.print("Enter the song title: ");
+        String songTitle = scanner.nextLine();
+
+        System.out.print("Enter the artist: ");
+        String artist = scanner.nextLine();
+
+        System.out.println("\n----------------\n");
+        System.out.println("SONG DETAILS");
+        System.out.println("\n----------------\n");
+        System.out.println("Year Released: " + year);
+        System.out.println("Genre: " + genre);
+        System.out.println("Album: " + album);
+        System.out.println("Song Title: " +  songTitle);
+        System.out.println("Artist: " + artist);
+        scanner.close();
+    }
+}
